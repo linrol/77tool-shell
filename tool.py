2df2fdc2a163572df81e00416a87f558b86f8c89
linrol <linrolmail@gmail.com>
2023-07-02 03:09:56 +0800

    77tool工具mr逻辑优化，保持和idea插件一致

--- tool.py
+++ tool.py
@@ -19,21 +19,27 @@ def cmd(command):
 
 
 @click.command(name="mr")
-@click.option("-c", "--commit", is_flag=True, help="是否自动提交")
-def create_mr(commit):
+def create_mr():
     """
     对本地当前分支的已commit未push的代码发起merge request
     """
+    _, is_git_repo = cmd("git rev-parse --is-inside-work-tree")
+    if is_git_repo != "true":
+        click.secho("你当前所在好像不是git工程目录，请切换目录后重试", fg="red")
+        sys.exit(-1)
     _, branch = cmd("git branch --show-current")
+    if len(branch) < 1:
+        click.secho("获取工程当前分支失败", fg="red")
+        sys.exit(-1)
     commit_size = len(cmd("git status -s")[1])
-    if commit and commit_size > 0:
-        commit_title = click.prompt("Input Commit Message:")
+    if commit_size > 0:
+        commit_title = click.prompt("\nInput Commit Message")
         cmd('git add .;git commit -m "{}"'.format(commit_title))
     push_size = len(cmd("git log {} ^origin".format(branch))[1])
     if push_size == 0:
         click.secho("当前没有需要push的代码!!!", fg="red")
         sys.exit(-1)
-    tmp_name = datetime.now().strftime("%Y%m%d%H%M")
+    tmp_name = datetime.now().strftime("%Y%m%d%H%M%S")
     gen_mr = "git push origin head:{} -o merge_request.target={} ".format(tmp_name, branch)
     gen_mr += "-o merge_request.create -o merge_request.remove_source_branch -f"
     [_, ret] = cmd(gen_mr)
